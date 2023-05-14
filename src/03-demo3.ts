d8ba7f8398f155053521d7146defd183fe0dd612
unknown <rodrigozucchini1997@gmail.com>
2023-05-14 12:07:56 -0300

    fundamentos de ts

---
+++ src/03-demo3.ts
@@ -0,0 +1,2 @@
+let myProductName:string = 'Producto 1';
+let myProductPrice:number = 123;
---
+++ src/03-typing.ts
@@ -0,0 +1,10 @@
+(() => {
+  let myProductName:string = 'Producto 1';
+  let myProductPrice:number = 123;
+
+  myProductName = 'change';
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 100;
+})();
