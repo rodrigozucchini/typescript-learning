3178a0378240650d6117c1aa5c284da20f088550
unknown <rodrigozucchini1997@gmail.com>
2023-05-14 12:30:50 -0300

    numbers

---
+++ src/04-numbers.ts
@@ -0,0 +1,19 @@
+(()=> {
+  let productPrice:number = 100;
+  productPrice = + 1;
+  console.log("Product", productPrice);
+
+  let productInStock: number;
+
+  let discount = parseInt("123");
+  console.log("discount", discount);
+
+  let hex = 0xfff;
+  console.log("hex", hex);
+
+  let bin  = 0b101001;
+  console.log("bin", bin);
+
+  let myNumber: Number  = 10; // usa el de minuscula para numberos, mala practica
+  console.log("myNumber", myNumber);
+})();
